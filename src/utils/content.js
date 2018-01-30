28607b6bc3e14f1511f2b2de10b1297b34571f87
Julien BOCK <julien.bock@decathlon.com>
2018-01-30 12:44:53 +0100

    First commit layout creation

--- src/utils/content.js
+++ src/utils/content.js
@@ -4,25 +4,99 @@ class Content extends Component {
   render(){
     return(
       <div className="content-wrap">
-        <div className="best-sellers-test">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+        <div className="best-sellers-items">
+            <h3 className="best-sellers-title"> Best Sellers </h3>
+            <div className="best-sellers-row">
+              <div className="item-content">
+                <span className="title-item">Underpants</span>
+                <img src="https://www.decathlon.fr/media/823/8237717/big_595c9a2093c54b9e95c1818561369fc7.jpg"></img>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt</p>
+                <p className="price">9,99€</p>
+                <div className="item-button-zone">
+                  <button>Add to cart</button>
+                  <button>View details</button>
+                </div>
+            </div>
+            <div className="item-content">
+              <span className="title-item">Underpants</span>
+              <img src="https://www.decathlon.fr/media/823/8237717/big_595c9a2093c54b9e95c1818561369fc7.jpg"></img>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt</p>
+              <p className="price">9,99€</p>
+              <div className="item-button-zone">
+                <button>Add to cart</button>
+                <button>View details</button>
+              </div>
+          </div>
+            <div className="item-content">
+              <span className="title-item">Underpants</span>
+              <img src="https://www.decathlon.fr/media/823/8237717/big_595c9a2093c54b9e95c1818561369fc7.jpg"></img>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt</p>
+              <p className="price">9,99€</p>
+              <div className="item-button-zone">
+                <button>Add to cart</button>
+                <button>View details</button>
+              </div>
+            </div>
+            <div className="item-content">
+              <span className="title-item">Underpants</span>
+              <img src="https://www.decathlon.fr/media/823/8237717/big_595c9a2093c54b9e95c1818561369fc7.jpg"></img>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt</p>
+              <p className="price">9,99€</p>
+              <div className="item-button-zone">
+                <button>Add to cart</button>
+                <button>View details</button>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="Reviews-test">
           <h3>"Good retention to there underpants"</h3>
           <p>John Doe, <i>12/12/2017</i></p>
         </div>
         <div className="news-items-test">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <h3 className="best-sellers-title"> Best Sellers </h3>
+            <div className="best-sellers-row">
+              <div className="item-content">
+                <span className="title-item">Underpants</span>
+                <img src="https://www.decathlon.fr/media/823/8237717/big_595c9a2093c54b9e95c1818561369fc7.jpg"></img>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt</p>
+                <p className="price">9,99€</p>
+                <div className="item-button-zone">
+                  <button>Add to cart</button>
+                  <button>View details</button>
+                </div>
+            </div>
+            <div className="item-content">
+              <span className="title-item">Underpants</span>
+              <img src="https://www.decathlon.fr/media/823/8237717/big_595c9a2093c54b9e95c1818561369fc7.jpg"></img>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt</p>
+              <p className="price">9,99€</p>
+              <div className="item-button-zone">
+                <button>Add to cart</button>
+                <button>View details</button>
+              </div>
+          </div>
+            <div className="item-content">
+              <span className="title-item">Underpants</span>
+              <img src="https://www.decathlon.fr/media/823/8237717/big_595c9a2093c54b9e95c1818561369fc7.jpg"></img>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt</p>
+              <p className="price">9,99€</p>
+              <div className="item-button-zone">
+                <button>Add to cart</button>
+                <button>View details</button>
+              </div>
+            </div>
+            <div className="item-content">
+              <span className="title-item">Underpants</span>
+              <img src="https://www.decathlon.fr/media/823/8237717/big_595c9a2093c54b9e95c1818561369fc7.jpg"></img>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt</p>
+              <p className="price">9,99€</p>
+              <div className="item-button-zone">
+                <button>Add to cart</button>
+                <button>View details</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
--- src/utils/footer.js
+++ src/utils/footer.js
@@ -4,15 +4,38 @@ class Footer extends Component {
   render(){
     return(
       <div className="footer-wrap">
-
-        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
-
-        </p>
+        <div className="footer-left">
+          <ul>
+            <li className="visible">Home</li>
+            <li className="visible">Catalog</li>
+            <li className="visible">Login</li>
+            <li className="visible">Login</li>
+          </ul>
+        </div>
+        <div className="vertical-separator"></div>
+        <div className="footer-middle">
+          <div className="footer-review-item">
+            <h3>"Good retention to there underpants"</h3>
+            <p>John Doe, <i>12/12/2017</i></p>
+          </div>
+          <div className="footer-review-item">
+            <h3>"Good retention to there underpants"</h3>
+            <p>John Doe, <i>12/12/2017</i></p>
+          </div>
+          <div className="footer-review-item">
+            <h3>"Good retention to there underpants"</h3>
+            <p>John Doe, <i>12/12/2017</i></p>
+          </div>
+        </div>
+        <div className="vertical-separator"></div>
+        <div className="footer-right">
+          <ul>
+            <li className="visible">Home</li>
+            <li className="visible">Catalog</li>
+            <li className="visible">Login</li>
+            <li className="visible">Login</li>
+          </ul>
+        </div>
       </div>
     );
   }
