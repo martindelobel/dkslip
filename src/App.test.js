ea25d81d477a04863c3e9a7f0764fa05370585e7
Martin Delobel <martin.delobel@decathlon.com>
2018-01-30 17:20:57 +0100

    tests fixed

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { catalogActions } from "../store/catalog/actions.js";
+import { displayCatalog } from "../store/catalog/selectors.js";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
