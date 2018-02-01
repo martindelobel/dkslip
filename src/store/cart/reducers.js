551ae9b0e4d059971efa7f218ec2cd1753bdc4f7
Martin Delobel <martin.delobel@decathlon.com>
2018-02-01 13:40:48 +0100

    add to cart button is working

--- src/store/cart/reducers.js
+++ src/store/cart/reducers.js
@@ -1,31 +1,53 @@
-const initialState = [{id:"toto",quantity:3}];
+const initialState = { productList: [] };
 
 export default function cartReducer(state = initialState, action) {
-  switch (action.type){
-    case "PUSH" :
+  switch (action.type) {
+    case "PUSH":
       return {
         ...state,
-        productList : [action.data]
+        productList: [action.data]
       };
-    case "ADD_PRODUCTS" :
+    case "ADD_PRODUCTS":
       return {
         ...state,
-        productList:action.data
+        productList: [action.data]
       };
-    case 'ADD':
+    case "ADD":
+      const arrayFiltered = state.productList.filter(
+        product => product.id === action.data
+      );
+      if (arrayFiltered.length > 0 && action.data === arrayFiltered[0].id) {
+        console.log("l'article est deja dans le panier");
+        const productToUpdate = state.productList.filter(
+          product => product.id === action.data
+        );
+        const indexToUpdate = state.productList.indexOf(productToUpdate[0]);
+        const quantityUpdated = state.productList.slice();
+
+        quantityUpdated.splice(indexToUpdate, 1, {
+          id: action.data,
+          quantity: state.productList[indexToUpdate].quantity + 1
+        });
+
+        return {
+          ...state,
+          productList: quantityUpdated
+        };
+      } else {
+        return {
+          ...state,
+          productList: [{ id: action.data, quantity: 1 }]
+        };
+      }
+    case "SUB":
       return {
         ...state,
-        productList : [{id : 777,quantity:2}]
+        productList: [{ id: 777, quantity: 0 }]
       };
-    case 'SUB':
+    case "DELETE":
       return {
         ...state,
-        productList : [{id : 777,quantity:0}]
-      };
-    case 'DELETE':
-      return {
-        ...state,
-        productList : [{id : 777,quantity:0}]
+        productList: [{ id: 777, quantity: 0 }]
       };
     default:
       return state;
