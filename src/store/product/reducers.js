0b20ce75a862f0f46f5190c93a0fda3a273825cd
Julien BOCK <julien.bock@decathlon.com>
2018-02-02 10:38:27 +0100

    update design and cart function

--- src/store/product/reducers.js
+++ src/store/product/reducers.js
@@ -5,11 +5,9 @@ const initialState = {
 export default function productReducer(state = initialState, action) {
   switch (action.type) {
     case "GET_PRODUCT":
-      const tempArray = state.product.slice();
-      tempArray.push(action.data)
       return {
         ...state,
-        product: tempArray
+        product: action.data
       };
     default:
       return state;
